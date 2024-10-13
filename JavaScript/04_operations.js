4588e9fc6e8d39152f7f942aeb44770ec1d8915f
sourav-code <skumar4230@gmail.com>
2024-10-13 19:23:19 +0530

    video7 completed

---
+++ JavaScript/04_operations.js
@@ -0,0 +1,40 @@
+console.log("Operations in Javascript ");
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+//arithmetic operators
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3); //to write in power like 2***3 =8
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " sourav";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2); //will give 12
+// console.log(1 + "2"); //will give 12
+// console.log("1" + "2"); //will give 12
+// console.log("1" + 2 + 2); //122
+// console.log(2 + 2 + "3"); //43
+// i.e will do calculation first then concatenate with string
+// console.log(3 + ((4 * 5) % 3)); //not important
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+console.log(gameCounter); //100
+++gameCounter;
+console.log(gameCounter); //100
+gameCounter++;
+console.log(gameCounter); //101
+
+//at the end this content is not that hard nor that imp still video 7 done
